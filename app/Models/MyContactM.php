2b967f94958bd638883721b665d0a83db609a3e7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 20:20:43 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Models/MyContactM.php
+++ app/Models/MyContactM.php
@@ -9,6 +9,6 @@ class MyContactM extends Model
 {
     protected $table = 'my_contacts';
     protected $primaryKey = 'id';
-    protected $fillable = ['owner', 'contact_name', 'role'];
+    protected $fillable = ['owner', 'contact_name', 'short_note'];
     use HasFactory;
 }
