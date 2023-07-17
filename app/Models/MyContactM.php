c4b3e4ec60d99948820e55c3240789428752ae80
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 20:19:40 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Models/MyContactM.php
+++ app/Models/MyContactM.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class MyContactM extends Model
 {
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'email', 'role'];
     use HasFactory;
 }
