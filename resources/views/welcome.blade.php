d4b8696d6783ac2f33b060a17c80e38066bcddfe
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 14:02:39 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     @include('header')
 
     <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        {{-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="main-content">
                     <div class="container text-center main-btns">
@@ -23,9 +23,8 @@
                         </div>
                     </div>
                 </div>
-
             @endif            
-        </div>
+        </div> --}}
 
 
 
