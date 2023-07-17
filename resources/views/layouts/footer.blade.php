aaa6886514ff32871ab36af69f56471d6b1477e6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 18:16:22 +0530

    Update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -38,6 +38,8 @@
                     <ul>
                         <li><i class="fas fa-envelope-open"></i> jehankandy@gmail.com</li>
                         <li><i class="fas fa-mobile"></i> +94 711758851</li>
+                        <li><a href="" target="_blank"><i class="fab fa-github-alt"></i> github.com/jehankandy</a></li>
+                        <li><a href="" target="_blank"><i class="fab fa-github-alt"></i> github.com/jehankandy</a></li>
                     </ul>
                 </div>
             </div>
