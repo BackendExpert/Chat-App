bc416e2300a495368e84db8e4c3ed5a245ceb853
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 18:07:56 +0530

    UPdate

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,14 @@
 <footer>
-    
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="footer-title">
+                About Us
+            </div>
+            <div class="footer-body">
+                Online Chat Application
+            </div>
+        </div>
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4"></div>
+    </div>
 </footer>
