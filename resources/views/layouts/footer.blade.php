cee3a8340ae647f8269f6d0966777ab8baa1147d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 18:22:55 +0530

    Update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -45,6 +45,7 @@
                 </div>
             </div>
         </div>
+        <hr>
         <center>
             <b><i class="far fa-copyright"></i>Online Chat Application</b> | Developed By <b><a href="https://github.com/JehanKandy" target="_blank">JehanKandy 2021 - 2023</a></b> & <b><a href="https://github.com/BackendExpert" target="_blank">BackendExpert</a></b> | All Rights Reserved
         </center>
