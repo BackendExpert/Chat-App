adcd80150d7e56406af7cef06a61b14129983c3c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 14:15:55 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
             text-align: center;
             padding: 250px 0;
             background-image: url(https://wallpaperaccess.com/full/2115.jpg);
+            background-image: linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)), url('images/background.jpg');
             height: 100vh;
             background-position: center;
             background-repeat: no-repeat;
