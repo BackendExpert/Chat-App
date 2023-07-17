cc660c954e7b41f9827f5dcbadfc0a36ba0fbc15
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 20:18:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- database/migrations/2023_07_17_122154_create_my_contacts_table.php
+++ database/migrations/2023_07_17_122154_create_my_contacts_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('my_contacts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('owner');
+            $table->string('contact_name');
+            $table->string('short_note');
+            $table->timestamps();            
         });
     }
 
