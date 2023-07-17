bdc7cdb6741a3b1a26ea884843fb88f998d0dd53
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 18:09:00 +0530

    Update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,16 @@
 <footer>
-    <div class="row">
-        <div class="col-lg-4">
-            <div class="footer-title">
-                About Us
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="footer-title">
+                    About Us
+                </div>
+                <div class="footer-body">
+                    Online Chat Application
+                </div>
             </div>
-            <div class="footer-body">
-                Online Chat Application
-            </div>
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4"></div>
         </div>
-        <div class="col-lg-4"></div>
-        <div class="col-lg-4"></div>
     </div>
 </footer>
