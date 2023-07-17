fa0551d19d863281719ea7e023f82252452bb397
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 14:05:55 +0530

    Update

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -29,4 +29,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
+     <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 </head>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -17,6 +17,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
-
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
