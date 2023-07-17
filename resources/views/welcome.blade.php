023ac44a5fee892d0a48659a750d2367d7b5724b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 14:09:44 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
             text-align: center;
             padding: 40px 0;
             background-image: url(https://mcdn.wallpapersafari.com/medium/46/30/ObEGSN.jpg);
+            width: 100vh;
         }
     </style>
 
