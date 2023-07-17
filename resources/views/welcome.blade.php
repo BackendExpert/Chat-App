7f4939b0a28591de4b15a3a0fb7f0bbd93f673b3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 14:08:03 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 
     @include('header')
 
+    <style>
+        .home-content{
+            text-align: center;
+        }
+    </style>
+
     <body class="antialiased">
         {{-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
