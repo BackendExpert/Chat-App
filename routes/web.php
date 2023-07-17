64553355b826c7cece4be3cb9eb20164751db51d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 18:39:47 +0530

    Update

--- routes/web.php
+++ routes/web.php
@@ -40,4 +40,4 @@ Route::get('client', function(){
 })->name('client')->middleware('client');
 
 Route::resource("/users", UserController::class);
-Route::get('/newChat', [MyContactController::class])
+Route::get('/newChat', [MyContactController::class, 'view_contact'])->name('newChat');
