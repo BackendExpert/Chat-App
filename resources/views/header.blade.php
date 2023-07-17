93c0980ba826a7fe0a1ab858d1789efb783ca4a3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 14:06:53 +0530

    Update

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -29,5 +29,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
-     <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+     <link rel="icon" type="image/x-icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVLu6HrTkF20KU1ZwOttuqptig4UXVD1JsPg&usqp=CAU">
 </head>
