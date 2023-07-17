aad76aabf247ea5995999d17a4abd60850bb5f9c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 14:26:36 +0530

    Update

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -94,7 +94,7 @@
 <div class="d-flex" id="wrapper">
     <!-- Sidebar -->
     <div class="bg-white" id="sidebar-wrapper">
-        <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i class="fa-sharp fa-solid fas fa-rss"></i></div>
+        <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i class="fa-sharp fa-solid fas fa-rss"></i> Online Chat</div>
         <div class="list-group list-group-flush my-3">
             <a href="{{url('/admin')}}" class="list-group-item list-group-item-action bg-transparent second-text active"><i
                     class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
