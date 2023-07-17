068bba9ddb824c32cbc1b32e81e1c851ab63bb69
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-17 19:51:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/myContacts/oneContact.blade.php
+++ resources/views/myContacts/oneContact.blade.php
@@ -2,61 +2,6 @@
 
 @extends('header')
 
-<style>
-    .contact-content{
-        padding: 40px 0;
-    }
-    .one-contact{
-        padding: 20px 30px;
-        border: 2px solid rgb(207, 207, 207);
-        border-radius: 5px;
-    }
-</style>
-
-<div class="contact-content">
-    <div class="container">
-        <a href="{{ url('/') }}"><button class="btn btn-primary">Back</button></a>
-        <h2 style="text-align: center;">All Contacts</h2><br>
-        @foreach ($new_contact as $contact)
-            <div class="one-contact">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <h3>{{ $contact->name }}</h3>
-                    </div>
-                    <div class="col-lg-6" style="text-align: right;">
-                        @php
-                            if($contact->name == Auth::user()->name){
-                                echo "<h4>Me</h4>";
-                            }
-                            else{
-                                @endphp
-                                    <a href="{{ url('/newChat/' . $contact->id) }}"><button class="btn btn-success">Add to Contact</button></a>
-                                @php
-                            }
-                        @endphp                        
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <p>
-                            @php
-                                if($contact->role == 1){
-                                    echo "<span class='badge bg-danger'>Admin</span>";
-                                }
-                                elseif($contact->role == 2){
-                                    echo "<span class='badge bg-warning'>Staff Member</span>";
-                                }
-                                elseif($contact->role == 3){
-                                    echo "<span class='badge bg-success'>Client</span>";
-                                }
-                            @endphp    
-                        </p>
-                    </div>
-                </div>
-            </div><br>
-        @endforeach
-    </div>
-</div>
 
 @include('layouts.footer')
 
