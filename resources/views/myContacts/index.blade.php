61fbe40158b3d6328c65b2f93a4245b24480dada
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-18 13:20:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/myContacts/index.blade.php
+++ resources/views/myContacts/index.blade.php
@@ -34,7 +34,7 @@
                     </div>
                     <div class="row">
                         <div class="col-lg-12">
-
+                            
                         </div>
                     </div>
                   </div>
