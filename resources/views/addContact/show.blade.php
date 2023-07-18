8d20c9c74d49416abb4795d5b22ff177833c19be
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-18 07:33:07 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/addContact/show.blade.php
+++ resources/views/addContact/show.blade.php
@@ -20,23 +20,22 @@
                     header("location:newChat");
                 @endphp
             @else
-                
+            
+                <form action="{{ url('newChat') }}" method="post">
+                    @csrf
+
+                    {{-- <p>{{ $one_contact->name }}</p> --}}
+
+                    <label for="contacter">Contact Name : </label>
+                    <input type="text" name="contact_name" id="" class="form-control" value="{{ $one_contact->name }}" disabled>
+                    <br>
+                    <label for="note">Note :</label>
+                    <input type="text" name="short_note" id="" class="form-control">
+                    <br>
+                    <input type="submit" value="Add To Contact" class="btn btn-success">
+                </form>
+
             @endif
-
-
-            <form action="{{ url('newChat') }}" method="post">
-                @csrf
-
-                {{-- <p>{{ $one_contact->name }}</p> --}}
-
-                <label for="contacter">Contact Name : </label>
-                <input type="text" name="contact_name" id="" class="form-control" value="{{ $one_contact->name }}" disabled>
-                <br>
-                <label for="note">Note :</label>
-                <input type="text" name="short_note" id="" class="form-control">
-                <br>
-                <input type="submit" value="Add To Contact" class="btn btn-success">
-            </form>
         </div>
     </div>
 
