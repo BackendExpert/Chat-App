4149fddcad05f636947ea9e6dd694d24f022e04b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-18 08:08:02 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/myContacts/index.blade.php
+++ resources/views/myContacts/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.dash')
 
 @section('content')
-    
+    <div class="admin-card">
+        <div class="container">
+            My Contact
+        </div>
+    </div>
 
 @endsection
 
