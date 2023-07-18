9428a6a465da2ceb99e32cb6b8c754399b534942
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-18 13:10:41 +0530

    Update

--- resources/views/myContacts/index.blade.php
+++ resources/views/myContacts/index.blade.php
@@ -21,10 +21,14 @@
                         <h3>{{ $contact->contact_name }}</h3>
                     </div>
                     <div class="col-lg-2">
-
+                        
                     </div>
                     <div class="col-lg-2">
-                        
+                        <form method="POST" action="{{ url('/videos' . '/' . $vid->id) }}" accept-charset="UTF-8" style="display:inline">
+                            {{ method_field('DELETE') }}
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-danger btn-sm" title="Delete video" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fas fa-calendar-times" aria-hidden="true"></i> Delete</button>
+                        </form>  
                     </div>
                 </div>
                 <br>
