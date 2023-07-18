e508ee07b321280355789798534934c6520dca74
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-18 07:37:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/addContact/show.blade.php
+++ resources/views/addContact/show.blade.php
@@ -25,6 +25,7 @@
                     {{-- <p>{{ $one_contact->name }}</p> --}}
 
                     <label for="contacter">Contact Name : </label>
+                    <input type="hidden" name="owner">
                     <input type="text" name="contact_name" id="" class="form-control" value="{{ $one_contact->name }}" disabled>
                     <br>
                     <label for="note">Note :</label>
