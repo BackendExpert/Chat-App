2e913bd459d888ea298e4d053db0fa3c5fc9c4fd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-18 07:56:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/addContact/show.blade.php
+++ resources/views/addContact/show.blade.php
@@ -19,6 +19,18 @@
             <script>window.location = "/newChat";</script>
             @else
             
+            @if ($errors->any())
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <strong>Errors : </strong> You got Following Errors while validation this form<br>
+                            <b>{{ $error }}</b>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endforeach
+                </ul>
+            @endif
+
                 <form action="{{ url('newChat') }}" method="post">
                     @csrf
 
